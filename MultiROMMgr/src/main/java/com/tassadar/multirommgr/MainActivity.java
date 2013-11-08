5e0409ab8523ac1d84420a6ce6d2d8fe35c9f532
Vojtech Bocek <vbocek@gmail.com>
2013-11-08 14:36:09 +0100

    [17] Re-enable CardUI slide-in on refresh

--- MultiROMMgr/src/main/java/com/tassadar/multirommgr/MainActivity.java
+++ MultiROMMgr/src/main/java/com/tassadar/multirommgr/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends Activity implements StatusAsyncTask.StatusAsyn
         UbuntuManifestAsyncTask.destroy();
 
         mCardView.clearCards();
+        mCardView.setSlideIn(true);
         start();
     }
 
