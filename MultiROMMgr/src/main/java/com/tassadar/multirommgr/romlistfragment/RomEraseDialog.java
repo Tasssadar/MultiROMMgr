85e1785fec98e80823258a0280410a89ea051de9
Vojtech Bocek <vbocek@gmail.com>
2013-12-29 20:44:50 +0100

    Add license notices into all files

--- MultiROMMgr/src/main/java/com/tassadar/multirommgr/romlistfragment/RomEraseDialog.java
+++ MultiROMMgr/src/main/java/com/tassadar/multirommgr/romlistfragment/RomEraseDialog.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of MultiROM Manager.
+ *
+ * MultiROM Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MultiROM Manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MultiROM Manager. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.tassadar.multirommgr.romlistfragment;
 
 import android.app.Activity;
@@ -9,7 +26,6 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 import com.tassadar.multirommgr.MainActivity;
